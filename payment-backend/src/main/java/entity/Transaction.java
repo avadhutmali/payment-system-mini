72f074a8137fc75d92642c27889549d63092ed63
avadhutmali <mali.avadhut7@gmail.com>
2026-01-19 15:19:17 +0530

    feat: add core database schema and JPA entities

---
+++ payment-backend/src/main/java/entity/Transaction.java
@@ -0,0 +1,50 @@
+package entity;
+
+import entity.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(
+        name = "Transaction",
+        indexes = @Index(
+                name = "idx_transaction_idempotency",
+                columnList = "idempotencyKey",
+                unique = true
+        )
+)
+public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID payerAccountId;
+
+    @Column(nullable = false)
+    private UUID payeeAccountId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountStatus status;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false,unique = true)
+    private String idempotencyKey;
+
+    @Column(nullable = false,updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = Instant.now();
+    }
+
+}
---
+++ payment-backend/src/main/java/entity/enums/LedgerEntryType.java
@@ -0,0 +1,6 @@
+package entity.enums;
+
+public enum LedgerEntryType {
+    DEBIT,
+    CREDIT
+}
